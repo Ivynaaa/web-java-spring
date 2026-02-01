b9c8470dbb0fe654e7f73b716c5b715bf729c717
Ivyna Alves <ivynamagalhaes003@gmail.com>
2026-02-01 19:32:47 -0300

    Adiciona serviço de API - axios e fetch

---
+++ code/tickets/frontend/src/services/api.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const SERVER = 'http://localhost:8080'
+
+const api = axios.create({
+    baseURL: SERVER
+})
+
+const api_fetch = async (endpoint: string,
+                            confg?: RequestInit
+) => {
+    const  result = await fetch(SERVER + endpoint, confg)
+    return await result.json()
+}
+
+export default api
+export {api_fetch}
